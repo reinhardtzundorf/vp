e1d864dbaf3ffecfe5be80de679923f85e63bac3
Reinhardt Zundorf <reinhardt.zundorf@spinningyourweb.net>
2017-03-15 11:54:30 +0200

    Saving changes.

--- modules/helpers/activate.php
+++ modules/helpers/activate.php
@@ -1,30 +1,41 @@
 <?php
-require('includes/config.php');
+	require('includes/config.php');
 
-//collect values from the url
-$memberID = trim($_GET['x']);
-$active = trim($_GET['y']);
+	/** ---------------------------------------------------------------------- **
+	 *	Collect values from the URL 											*
+	 ** ---------------------------------------------------------------------- **/
+	$memberID = trim($_GET['x']);
+	$active = trim($_GET['y']);
 
-//if id is number and the active token is not empty carry on
-if(is_numeric($memberID) && !empty($active)){
+	/** ---------------------------------------------------------------------- **
+	 *	If id is number and the active token is not empty carry on 				*
+	 ** ---------------------------------------------------------------------- **/
+	if(is_numeric($memberID) && !empty($active))
+	{
+		/** ------------------------------------------------------------------ **
+		 *	Update user's record set the active column to Yes where the 		*
+		 *	memberID and active value match the ones provided in the array 		*
+		 ** ------------------------------------------------------------------ **/
+		$stmt = $db->prepare("UPDATE members 
+							  SET active 	 = 'Yes' 
+							  WHERE memberID = :memberID 
+							  AND active 	 = :active");
 
-	//update users record set the active column to Yes where the memberID and active value match the ones provided in the array
-	$stmt = $db->prepare("UPDATE members SET active = 'Yes' WHERE memberID = :memberID AND active = :active");
-	$stmt->execute(array(
-		':memberID' => $memberID,
-		':active' => $active
-	));
+		$stmt->execute(array(':memberID' => $memberID,
+  							 ':active' 	 => $active		));
 
-	//if the row was updated redirect the user
-	if($stmt->rowCount() == 1){
-
-		//redirect to login page
-		header('Location: login.php?action=active');
-		exit;
-
-	} else {
-		echo "Your account could not be activated."; 
+		/** ------------------------------------------------------------------- **
+		 *	If the result fetches a result (1)- activate the account. 			 *
+		 ** ------------------------------------------------------------------- **/
+		if($stmt->rowCount() == 1)
+		{
+			header('Location: login.php?action=active');
+			exit;
+		} 
+		else 
+		{
+			echo "Your account could not be activated."; 
+		}
+		
 	}
-	
-}
 ?>
--- modules/layout/layout/header.php
+++ modules/layout/layout/header.php
@@ -1,9 +1,43 @@
+<!-- Header.php -->
 <!DOCTYPE html>
+
+<!-- HTML -->
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title><?php if(isset($title)){ echo $title; }?></title>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style/main.css">
-</head>
-<body>
+
+	<!-- Head -->
+	<head>
+		<!-- Meta charset -->
+	    <meta charset="utf-8"><!-- /meta -->
+
+	    <!-- title -->
+	    <title>
+	    <?php 
+
+	   		/** ------------------------------------------------------------------ **
+	   		 *	Set the current page's title, if '$title' does not have a value 	*
+	   		 *  then set the default value. 										*
+	   		 ** ------------------------------------------------------------------ **/
+	    	if(isset($title)) 
+	    	{ 
+	    		echo $title; 
+	    	}
+	    	else
+	    	{
+	    		echo "Volunteers Portal | Insight Student Volunteers";
+	    	}
+		?>
+	    </title><!-- /title -->
+
+	    <!-- CSS -->
+	    <link rel="stylesheet" href="../../../res/css/bootstrap.css">		<!-- Bootstrap -->
+	    <link rel="stylesheet" href="../../../res/css/bootstrap-theme.css"> <!-- Bootstrap-Theme -->
+	    <link rel="stylesheet" href="../../../res/css/font-awesome.min.css"><!-- Font-Awesome -->
+	    <link rel="stylesheet" href="../../../res/css/style/main.css">		<!-- Portal Style -->
+	    <!-- /css -->
+
+	</head><!-- /head -->
+
+	<!-- Body -->
+	<body>
+
+	<!-- Content for page is inserted here... -->
